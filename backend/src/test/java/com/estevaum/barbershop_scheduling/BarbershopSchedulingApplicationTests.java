8fb7594c2c5c5eaa6d7fc1a802a926398fcdb9ff
aluno <aluno@embarquedigital.portodigital.local>
2025-04-03 17:57:06 -0300

    App initialization

---
+++ backend/src/test/java/com/estevaum/barbershop_scheduling/BarbershopSchedulingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.estevaum.barbershop_scheduling;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BarbershopSchedulingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
